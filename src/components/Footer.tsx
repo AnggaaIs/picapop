4c9b23940597e8a250eb33d0ade04b4dbc116362
AnggaaIs <servantangga@gmail.com>
2025-03-08 17:48:36 +0700

    fix: update footer to include GitHub link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,23 @@ export default function Footer() {
   return (
     <footer className="footer sm:footer-horizontal rounded-t-xl footer-center bg-base-200 text-base-content p-4">
       <aside>
-        <p>Copyright © {new Date().getFullYear()} - All right reserved by LazyPeople.</p>
+        <p>
+          Copyright © {new Date().getFullYear()} - All rights reserved by
+          LazyPeople.
+        </p>
+        <p>
+          Open source on{" "}
+          <a
+            href="https://github.com/AnggaaIs/photobox"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline font-bold"
+          >
+            GitHub
+          </a>
+          .
+        </p>
       </aside>
     </footer>
-  )
+  );
 }
