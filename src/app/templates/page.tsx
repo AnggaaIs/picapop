ba4c6b1eee719d0db92ee3c6c12d4544bd16e383
jeremy776 <jeremy.kusuma2018@gmail.com>
2025-03-28 11:20:18 +0700

    fix: adjust padding in template layout for better alignment

--- src/app/templates/page.tsx
+++ src/app/templates/page.tsx
@@ -7,11 +7,9 @@ export const metadata: Metadata = {
 };
 
 export default function Template() {
-
-
   return <>
     <div className="flex flex-col items-center py-10 mb-20 min-h-screen">
-      <div className="flex flex-col items-center max-w-6xl md:py-5 rounded-xl w-full">
+      <div className="flex px-5 flex-col items-center max-w-6xl md:py-5 rounded-xl w-full">
         <h1 className="text-center text-4xl font-semibold">Template Foto</h1>
         <p className="text-center">
           Kami menyediakan beberapa template foto yang bisa kamu gunakan
