1dded0595320bd6dcc9b91030dc4efb4fc73b22c
jeremy776 <jeremy.kusuma2018@gmail.com>
2025-04-01 12:51:28 +0700

    feat: implement custom 404 page and robots.txt functionality; update sitemap entries

---
+++ src/app/not-found.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <section className="min-h-[100vh] h-full">
+      <div className="flex items-center justify-center px-4 py-8 mx-auto sm:py-16 lg:py-24 lg:px-6 h-full">
+        <div className="mx-auto max-w-screen-sm text-center">
+          <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">404</h1>
+          <p className="mb-4 text-3xl tracking-tight font-bold">Something&#39;s missing.</p>
+          <p className="mb-4 text-lg font-light">Sorry, we can&rsquo;t find that page. You&#39;ll find lots to explore on the home page. </p>
+          <Link href="/" className="inline-flex text-white focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4">Back to Homepage</Link>
+        </div>
+      </div>
+    </section>
+  )
+}
