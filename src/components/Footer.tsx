8ef36472728872cfcb73002ab6ba3db36fbbf8b6
jeremy776 <jeremy.kusuma2018@gmail.com>
2025-04-28 17:06:50 +0700

    refactor: update Footer component layout and styling for improved presentation

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,24 +1,23 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
-    <footer className="footer sm:footer-horizontal rounded-t-xl footer-center bg-base-200 text-base-content p-4">
-      <aside className="-space-y-2">
-        <p>
-          Copyright © {new Date().getFullYear()} - All rights reserved by
-          LazyPeople.
-        </p>
-        <p>
-          Open source on{" "}
-          <a
-            href="https://github.com/AnggaaIs/picapop"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="font-bold hover:underline transition-all ease-linear"
-          >
-            GitHub
-          </a>
-          .
-        </p>
-      </aside>
+    <footer className="p-4 bg-base-200 rounded-t-3xl md:p-8 lg:p-10">
+      <div className="mx-auto max-w-screen-xl text-center">
+        <Link href="/" className="flex justify-center items-center text-2xl font-semibold text-gray-900 dark:text-white">
+          PicaPop
+        </Link>
+        <p className="my-6 text-gray-500 dark:text-gray-400">Pilih template, ambil foto, dan bagikan hasilnya dalam sekejap!</p>
+        <ul className="flex flex-wrap justify-center items-center mb-6 text-gray-900 dark:text-white">
+          <li>
+            <a href="/privacy-policy" className="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+          </li>
+          <li>
+            <a href="/terms-of-use" className="mr-4 hover:underline md:mr-6">Terms of Use</a>
+          </li>
+        </ul>
+        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="https://lazypeople.my.id" className="hover:underline">LazyPeople org</a>. All Rights Reserved.</span>
+      </div>
     </footer>
   );
 }
