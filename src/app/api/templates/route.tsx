47407e277bae2b5b882c9f247d015e91572a39b2
jeremy776 <jeremy.kusuma2018@gmail.com>
2025-05-18 18:17:23 +0700

    refactor: remove commented-out code for template processing

--- src/app/api/templates/route.tsx
+++ src/app/api/templates/route.tsx
@@ -20,27 +20,6 @@ export async function GET() {
 
     const files = fs.readdirSync(templateDir);
 
-    // const templates = files
-    //   .filter((file) => file.endsWith(".png"))
-    //   .map((file) => {
-    //     const namaFile = file.replace(".png", "").replace(/-/g, " ");
-    //     const label = namaFile.replace(/\s*\(\d{2} \d{2} \d{4}\)$/, "");
-    //     const dateMatch = namaFile.match(/\((\d{2}) (\d{2}) (\d{4})\)/);
-    //     let date;
-    //     if (dateMatch) {
-    //       const day = parseInt(dateMatch[1], 10);
-    //       const month = parseInt(dateMatch[2], 10) - 1;
-    //       const year = parseInt(dateMatch[3], 10);
-
-    //       date = new Date(year, month, day);
-    //     }
-    //     return {
-    //       label,
-    //       date,
-    //       filename: file,
-    //     };
-    //   });
-
     const templates = files
       .filter((file) => /\.(png|jpg)$/i.test(file)) // support .png dan .jpg (case-insensitive)
       .map((file) => {
